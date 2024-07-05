61db733ef05557069dc9fa1fd2c550d1b12ce4a7
lisapeters13 <lisapetersirl@gmail.com>
2024-07-05 18:33:38 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // prevent the default behavior of form submission
+const input = document.querySelector('#new-task-description ')
+    console.log (input.value) // handle form submission logic here
+
+    const newli = document.createElement('li')
+    newli.textContent = input.value
+    const ul = document.querySelector('#tasks')
+    ul.append(newli)
+
+    const DeleteButton = document.createElement('button')
+    DeleteButton.textContent = 'x'
+    newli.append(DeleteButton)
+    DeleteButton.addEventListener('click',Delete)
+    function Delete(){
+      newli.remove()
+    }
+ 
